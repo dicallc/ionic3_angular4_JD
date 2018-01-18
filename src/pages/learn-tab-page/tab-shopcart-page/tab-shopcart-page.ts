971683294caa6ae95bde7d66baeab185fa8864ef
jiangdk <jiangdk@e-lead.cn>
2018-01-18 10:25:58 +0800

    添加购物车的逻辑

--- src/pages/learn-tab-page/tab-shopcart-page/tab-shopcart-page.ts
+++ src/pages/learn-tab-page/tab-shopcart-page/tab-shopcart-page.ts
@@ -1,36 +1,108 @@
 import {Component} from '@angular/core';
-import { NavController, NavParams} from 'ionic-angular';
+import {NavController, NavParams} from 'ionic-angular';
 import {LookService} from "../../../providers/look-service";
-import {GOODLIST_head_URL} from "../../../providers/Constants";
+import {AlertController} from 'ionic-angular';
 
-/**
- * Generated class for the TabMessagesPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @Component({
   selector: 'page-tab-messages-page',
   templateUrl: 'tab-shopcart-page.html',
 })
 export class TabMessagesPage {
-  obj_ShopCartRecommendData = [];
-  private errorMessage: String;
-  Image_head_Url: string;
+  shopcart;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public lookservice: LookService) {
-    this.Image_head_Url=GOODLIST_head_URL;
+  constructor(public alertCtrl: AlertController, public navCtrl: NavController, public navParams: NavParams, public lookservice: LookService) {
+    this.shopcart = [{
+      shops: {
+        shop_name: '小红书福利社',
+        isSelect: false,
+        id: '0',
+        shop_type: '自营',
+        allPrise: 4668,
+        products: [
+          {
+            name: '资生堂防晒霜 60ml',
+            price: 199,
+            num: 2
+          },
+          {
+            name: '兰蔻精华液 100ml',
+            price: 854,
+            num: 5
+          }]
+      }
+
+    }];
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad TabMessagesPage');
-    this.lookservice.getGoodsList().subscribe(
-      countries => this.obj_ShopCartRecommendData = <any>countries,
-      error => this.errorMessage = <any>error,
-      function complete() {
 
-      },
-    );
+  }
+
+  AddRemoveRecipeToFavorite(item, index) {
+    console.log(index)
+  }
+
+  /**
+   * 商品添加
+   */
+  addNum(index, item_index) {
+    if (this.shopcart[index].shops.products[item_index].num == 0) return
+    ++this.shopcart[index].shops.products[item_index].num;
+    this.numAllPrice();
+  }
+
+  /**
+   * 商品减少
+   */
+  reduceNum(index, item_index) {
+    if (this.shopcart[index].shops.products[item_index].num == 0) return
+    --this.shopcart[index].shops.products[item_index].num;
+    this.numAllPrice();
+  }
+
+  /**
+   * 计算所有商品价格
+   */
+  numAllPrice() {
+    for (let i = 0; i < this.shopcart.length; i++) {
+      let shops = this.shopcart[i].shops;
+      let finalprise = 0;
+      for (let j = 0; j < shops.products.length; j++) {
+        finalprise += (shops.products[j].num * shops.products[j].price);
+      }
+      this.shopcart[i].shops.allPrise = finalprise;
+    }
+    console.log(this.shopcart[0].shops.allPrise);
+  }
+
+  deleteProduct(index, item_index) {
+    this.showConfirm(index, item_index);
+  }
+
+  /**
+   * 弹出提示
+   */
+  showConfirm(index, item_index) {
+    let confirm = this.alertCtrl.create({
+      title: '温馨提示',
+      message: '是否要删除该商品?',
+      buttons: [
+        {
+          text: '再看看',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: '是的',
+          handler: () => {
+            this.shopcart[index].shops.products.splice(item_index, 1);
+            this.numAllPrice();
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 
 }
